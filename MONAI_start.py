90ab49bb87c3349b6b54ce9ec5b19e749ce32b3f
Bishnu Paudel <paduelb@cardiff.ac.uk>
2021-12-22 00:33:17 +0000

    aded contents from tutorial - fingers crossed will work without hiccup in SCW

--- MONAI_start.py
+++ MONAI_start.py
@@ -45,6 +45,8 @@ from monai.metrics import DiceMetric
 from monai.inferers import sliding_window_inference
 from monai.data import DataLoader
 import re
+from monai.networks.nets import SegResNet
+import time
 
 IMG_EXTENSIONS = [
     '.jpg', '.JPG', '.jpeg', '.JPEG',
@@ -180,22 +182,306 @@ val_dataset=BratsDataset("../RSNA_ASNR_MICCAI_BraTS2021_ValidationData",transfor
 train_loader=DataLoader(train_dataset, batch_size=1, shuffle=True)
 val_loader=DataLoader(val_dataset, batch_size=1, shuffle=True)
 
-# pick one image from DecathlonDataset to visualize and check the 4 channels
-print(f"image shape: {val_dataset[2]['image'].shape}")
-plt.figure("image", (24, 6))
-for i in range(4):
-    plt.subplot(1, 4, i + 1)
-    plt.title(f"image channel {i}")
-    plt.imshow(val_dataset[2]["image"][i, :, :, 60].detach().cpu(), cmap="gray")
-plt.show()
-# also visualize the 3 channels mask corresponding to this image
-print(f"mask shape: {val_dataset[2]['mask'].shape}")
-plt.figure("mask", (18, 6))
-for i in range(3):
-    plt.subplot(1, 3, i + 1)
-    plt.title(f"mask channel {i}")
-    plt.imshow(val_dataset[2]["mask"][i, :, :, 60].detach().cpu())
-plt.show()
+# # pick one image from DecathlonDataset to visualize and check the 4 channels
+# print(f"image shape: {val_dataset[2]['image'].shape}")
+# plt.figure("image", (24, 6))
+# for i in range(4):
+#     plt.subplot(1, 4, i + 1)
+#     plt.title(f"image channel {i}")
+#     plt.imshow(val_dataset[2]["image"][i, :, :, 60].detach().cpu(), cmap="gray")
+# plt.show()
+# # also visualize the 3 channels mask corresponding to this image
+# print(f"mask shape: {val_dataset[2]['mask'].shape}")
+# plt.figure("mask", (18, 6))
+# for i in range(3):
+#     plt.subplot(1, 3, i + 1)
+#     plt.title(f"mask channel {i}")
+#     plt.imshow(val_dataset[2]["mask"][i, :, :, 60].detach().cpu())
+# plt.show()
+
+root_dir="./"
+
+max_epochs = 300
+val_interval = 1
+VAL_AMP = True
+
+# standard PyTorch program style: create SegResNet, DiceLoss and Adam optimizer
+device = torch.device("cuda:0")
+model = SegResNet(
+    blocks_down=[1, 2, 2, 4],
+    blocks_up=[1, 1, 1],
+    init_filters=32,
+    in_channels=4,
+    out_channels=3,
+    dropout_prob=0.2,
+).to(device)
+loss_function = DiceLoss(smooth_nr=0, smooth_dr=1e-5, squared_pred=True, to_onehot_y=False, sigmoid=True)
+optimizer = torch.optim.Adam(model.parameters(), 1e-4, weight_decay=1e-5)
+lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs)
+
+dice_metric = DiceMetric(include_background=True, reduction="mean")
+dice_metric_batch = DiceMetric(include_background=True, reduction="mean_batch")
+
+post_trans = Compose(
+    [EnsureType(), Activations(sigmoid=True), AsDiscrete(threshold=0.5)]
+)
+
+
+# define inference method
+def inference(input):
+
+    def _compute(input):
+        return sliding_window_inference(
+            inputs=input,
+            roi_size=(240, 240, 160),
+            sw_batch_size=1,
+            predictor=model,
+            overlap=0.5,
+        )
+
+    if VAL_AMP:
+        with torch.cuda.amp.autocast():
+            return _compute(input)
+    else:
+        return _compute(input)
+
+
+# use amp to accelerate training
+scaler = torch.cuda.amp.GradScaler()
+# enable cuDNN benchmark
+torch.backends.cudnn.benchmark = True
+
+best_metric = -1
+best_metric_epoch = -1
+best_metrics_epochs_and_time = [[], [], []]
+epoch_loss_values = []
+metric_values = []
+metric_values_tc = []
+metric_values_wt = []
+metric_values_et = []
+
+total_start = time.time()
+for epoch in range(max_epochs):
+    epoch_start = time.time()
+    print("-" * 10)
+    print(f"epoch {epoch + 1}/{max_epochs}")
+    model.train()
+    epoch_loss = 0
+    step = 0
+    for batch_data in train_loader:
+        step_start = time.time()
+        step += 1
+        inputs, masks = (
+            batch_data["image"].to(device),
+            batch_data["mask"].to(device),
+        )
+        optimizer.zero_grad()
+        with torch.cuda.amp.autocast():
+            outputs = model(inputs)
+            loss = loss_function(outputs, masks)
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+        epoch_loss += loss.item()
+        print(
+            f"{step}/{len(train_dataset) // train_loader.batch_size}"
+            f", train_loss: {loss.item():.4f}"
+            f", step time: {(time.time() - step_start):.4f}"
+        )
+    lr_scheduler.step()
+    epoch_loss /= step
+    epoch_loss_values.append(epoch_loss)
+    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+    if (epoch + 1) % val_interval == 0:
+        model.eval()
+        with torch.no_grad():
+
+            for val_data in val_loader:
+                val_inputs, val_masks = (
+                    val_data["image"].to(device),
+                    val_data["mask"].to(device),
+                )
+                val_outputs = inference(val_inputs)
+                val_outputs = [post_trans(i) for i in decollate_batch(val_outputs)]
+                dice_metric(y_pred=val_outputs, y=val_masks)
+                dice_metric_batch(y_pred=val_outputs, y=val_masks)
+
+            metric = dice_metric.aggregate().item()
+            metric_values.append(metric)
+            metric_batch = dice_metric_batch.aggregate()
+            metric_tc = metric_batch[0].item()
+            metric_values_tc.append(metric_tc)
+            metric_wt = metric_batch[1].item()
+            metric_values_wt.append(metric_wt)
+            metric_et = metric_batch[2].item()
+            metric_values_et.append(metric_et)
+            dice_metric.reset()
+            dice_metric_batch.reset()
+
+            if metric > best_metric:
+                best_metric = metric
+                best_metric_epoch = epoch + 1
+                best_metrics_epochs_and_time[0].append(best_metric)
+                best_metrics_epochs_and_time[1].append(best_metric_epoch)
+                best_metrics_epochs_and_time[2].append(time.time() - total_start)
+                torch.save(
+                    model.state_dict(),
+                    os.path.join(root_dir, "best_metric_model.pth"),
+                )
+                print("saved new best metric model")
+            print(
+                f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
+                f" tc: {metric_tc:.4f} wt: {metric_wt:.4f} et: {metric_et:.4f}"
+                f"\nbest mean dice: {best_metric:.4f}"
+                f" at epoch: {best_metric_epoch}"
+            )
+    print(f"time consuming of epoch {epoch + 1} is: {(time.time() - epoch_start):.4f}")
+total_time = time.time() - total_start
+
+print(f"train completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}, total time: {total_time}.")
+
+# ####~~~~~~~~~~~~~~~~PLOTS~~~~~~~~~~~~~~~~
+# plt.figure("train", (12, 6))
+# plt.subplot(1, 2, 1)
+# plt.title("Epoch Average Loss")
+# x = [i + 1 for i in range(len(epoch_loss_values))]
+# y = epoch_loss_values
+# plt.xlabel("epoch")
+# plt.plot(x, y, color="red")
+# plt.subplot(1, 2, 2)
+# plt.title("Val Mean Dice")
+# x = [val_interval * (i + 1) for i in range(len(metric_values))]
+# y = metric_values
+# plt.xlabel("epoch")
+# plt.plot(x, y, color="green")
+# plt.show()
+
+# plt.figure("train", (18, 6))
+# plt.subplot(1, 3, 1)
+# plt.title("Val Mean Dice TC")
+# x = [val_interval * (i + 1) for i in range(len(metric_values_tc))]
+# y = metric_values_tc
+# plt.xlabel("epoch")
+# plt.plot(x, y, color="blue")
+# plt.subplot(1, 3, 2)
+# plt.title("Val Mean Dice WT")
+# x = [val_interval * (i + 1) for i in range(len(metric_values_wt))]
+# y = metric_values_wt
+# plt.xlabel("epoch")
+# plt.plot(x, y, color="brown")
+# plt.subplot(1, 3, 3)
+# plt.title("Val Mean Dice ET")
+# x = [val_interval * (i + 1) for i in range(len(metric_values_et))]
+# y = metric_values_et
+# plt.xlabel("epoch")
+# plt.plot(x, y, color="purple")
+# plt.show()
+
+################## ~~~~~~~~~~~~~Best model output with image and label ~~~~~~~~~~ #######################
+# model.load_state_dict(
+#     torch.load(os.path.join(root_dir, "best_metric_model.pth"))
+# )
+# model.eval()
+# with torch.no_grad():
+#     # select one image to evaluate and visualize the model output
+#     val_input = val_dataset[6]["image"].unsqueeze(0).to(device)
+#     roi_size = (128, 128, 64)
+#     sw_batch_size = 4
+#     val_output = inference(val_input)
+#     val_output = post_trans(val_output[0])
+#     plt.figure("image", (24, 6))
+#     for i in range(4):
+#         plt.subplot(1, 4, i + 1)
+#         plt.title(f"image channel {i}")
+#         plt.imshow(val_dataset[6]["image"][i, :, :, 70].detach().cpu(), cmap="gray")
+#     plt.show()
+#     # visualize the 3 channels label corresponding to this image
+#     plt.figure("label", (18, 6))
+#     for i in range(3):
+#         plt.subplot(1, 3, i + 1)
+#         plt.title(f"label channel {i}")
+#         plt.imshow(val_dataset[6]["label"][i, :, :, 70].detach().cpu())
+#     plt.show()
+#     # visualize the 3 channels model output corresponding to this image
+#     plt.figure("output", (18, 6))
+#     for i in range(3):
+#         plt.subplot(1, 3, i + 1)
+#         plt.title(f"output channel {i}")
+#         plt.imshow(val_output[i, :, :, 70].detach().cpu())
+#     plt.show()
+
+########### ~~~~~~~~~~~~~~~~~~~~~~~ Evaluation ~~~~~~~~~~~~~~~~~~~~~FIX this
+
+# val_org_transforms = Compose(
+#     [
+#         LoadImaged(keys=["image", "label"]),
+#         EnsureChannelFirstd(keys=["image"]),
+#         ConvertToMultiChannelBasedOnBratsClassesd(keys="label"),
+#         Spacingd(keys=["image"], pixdim=(1.0, 1.0, 1.0), mode="bilinear"),
+#         Orientationd(keys=["image"], axcodes="RAS"),
+#         NormalizeIntensityd(keys="image", nonzero=True, channel_wise=True),
+#         EnsureTyped(keys=["image", "label"]),
+#     ]
+# )
+
+# val_org_ds = DecathlonDataset(
+#     root_dir=root_dir,
+#     task="Task01_BrainTumour",
+#     transform=val_org_transforms,
+#     section="validation",
+#     download=False,
+#     num_workers=4,
+#     cache_num=0,
+# )
+# val_org_loader = DataLoader(val_org_ds, batch_size=1, shuffle=False, num_workers=4)
+
+# post_transforms = Compose([
+#     EnsureTyped(keys="pred"),
+#     Invertd(
+#         keys="pred",
+#         transform=val_org_transforms,
+#         orig_keys="image",
+#         meta_keys="pred_meta_dict",
+#         orig_meta_keys="image_meta_dict",
+#         meta_key_postfix="meta_dict",
+#         nearest_interp=False,
+#         to_tensor=True,
+#     ),
+#     Activationsd(keys="pred", sigmoid=True),
+#     AsDiscreted(keys="pred", threshold=0.5),
+# ])
+
+# model.load_state_dict(torch.load(
+#     os.path.join(root_dir, "best_metric_model.pth")))
+# model.eval()
+
+# with torch.no_grad():
+#     for val_data in val_org_loader:
+#         val_inputs = val_data["image"].to(device)
+#         val_data["pred"] = inference(val_inputs)
+#         val_data = [post_transforms(i) for i in decollate_batch(val_data)]
+#         val_outputs, val_labels = from_engine(["pred", "label"])(val_data)
+#         dice_metric(y_pred=val_outputs, y=val_labels)
+#         dice_metric_batch(y_pred=val_outputs, y=val_labels)
+
+#     metric_org = dice_metric.aggregate().item()
+#     metric_batch_org = dice_metric_batch.aggregate()
+
+#     dice_metric.reset()
+#     dice_metric_batch.reset()
+
+# metric_tc, metric_wt, metric_et = metric_batch[0].item(), metric_batch[1].item(), metric_batch[2].item()
+
+# print("Metric on original image spacing: ", metric)
+# print(f"metric_tc: {metric_tc:.4f}")
+# print(f"metric_wt: {metric_wt:.4f}")
+# print(f"metric_et: {metric_et:.4f}")
+
+
+######################~~~~~~~~3D Seg End ~~~~~~###################
+
+
 
 # device=torch.device(("cuda:0"))
 # net=UNet(dimensions=2, in_channels=1, out_channels=3, channels=(16,32,64,128,256),strides=(2,2,2,2),num_res_units=2
