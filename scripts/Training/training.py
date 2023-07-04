a4a5ee05fc5b2d7fcc17ea709d106278ba36d2e7
bip5 <bip5@aber.ac.uk>
2023-07-04 21:00:58 +0100

    working partially for full dataset DDP

--- scripts/Training/training.py
+++ scripts/Training/training.py
@@ -24,7 +24,10 @@ model_name,
 T_max,
 lr,
 max_samples,
-model_name
+model_name,
+fs_ensemble,
+workers,
+unused_collection #flag for training with full dataset
 )
 from Evaluation.evaluation import (
 inference,
@@ -32,7 +35,7 @@ inference,
 )
 from Training.loss_function import loss_function
 from Training.network import model
-from Training.optimiser import optimiser
+
 
 from Evaluation.evaluation import (
 dice_metric,
@@ -42,19 +45,38 @@ inference,
 
 from Input.dataset import (
 BratsDataset,
+EnsembleDataset,
 train_indices,
 val_indices
 )
 
 from monai.utils import set_determinism
-from monai.data import decollate_batch
+from monai.data import decollate_batch,partition_dataset
 from datetime import date, datetime
 import time
 from torchsummary import summary
 from torch.utils.data import Subset
- 
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel
+from torch.utils.data.distributed import DistributedSampler
+torch.multiprocessing.set_sharing_strategy('file_system')
 
-    
+# import psutil
+# import threading
+
+
+# def print_memory():
+    # print(f'Allocated: {torch.cuda.memory_allocated() / 1024 ** 2:.2f} MB')
+    # print(f'Reserved:    {torch.cuda.memory_reserved() / 1024 ** 2:.2f} MB')
+    # process = psutil.Process(os.getpid())
+    # print(f'CPU memory : {process.memory_info().rss / 1024 ** 2:.2f} MB')
+
+# def schedule_prints(interval=10):  # 10 seconds by default
+    # threading.Timer(interval, schedule_prints, [interval]).start()
+    # print_memory()
+
+##Call this once before starting your training
+# schedule_prints()
 os.environ['PYTHONHASHSEED']=str(seed)
 torch.backends.cudnn.deterministic = True
 torch.manual_seed(seed)
@@ -62,28 +84,46 @@ np.random.seed(seed)
 torch.cuda.manual_seed(seed)
 set_determinism(seed=seed)
 
+local_rank = int(os.environ['LOCAL_RANK'])
+dist.init_process_group(backend="nccl", init_method="env://")
     
+  
 
-train_dataset=BratsDataset("/scratch/a.bip5/BraTS 2021/RSNA_ASNR_MICCAI_BraTS2021_TrainingData"  ,transform=train_transform )
-if CV_flag==1:
-    print("loading cross val data")
-    val_dataset=Subset(train_dataset,train_indices)
-    train_dataset=Subset(train_dataset,train_indices)
-    
-else:     
-    val_dataset=Subset(train_dataset,np.arange(10))
-    train_dataset=Subset(train_dataset,[10])    
-    
-print("number of files processed: ", train_dataset.__len__())
-train_loader=DataLoader(train_dataset, batch_size=batch_size, shuffle=True,num_workers=1)
-val_loader=DataLoader(val_dataset, batch_size=batch_size, shuffle=False,num_workers=4)
+if fs_ensemble == 1:
+    train_dataset = partition_dataset(data=EnsembleDataset('/scratch/a.bip5/BraTS 2021/selected_files_seed3.csv', transform=train_transform), shuffle=False, num_partitions=dist.get_world_size(), even_divisible=False)[dist.get_rank()]
+    val_dataset = partition_dataset(data=EnsembleDataset('/scratch/a.bip5/BraTS 2021/selected_files_seed4.csv', transform=train_transform), num_partitions=dist.get_world_size(), even_divisible=False)[dist.get_rank()]
+else:
+    full_dataset = BratsDataset("/scratch/a.bip5/BraTS 2021/RSNA_ASNR_MICCAI_BraTS2021_TrainingData", transform=train_transform)
+
+    if CV_flag == 1:
+        print("loading cross val data") # this is printed
+        val_indices = train_indices
+        train_indices = train_indices
+    else:     
+        val_indices = np.arange(10)
+        train_indices = [10]
+
+    train_dataset =Subset(full_dataset, train_indices)
+    val_dataset = Subset(full_dataset, val_indices)
+
+rank = int(os.getenv('RANK', '0'))
+world_size = int(os.getenv('WORLD_SIZE', '1'))
+print("number of files processed: ", train_dataset.__len__()) #this is not
+train_sampler = DistributedSampler(train_dataset, num_replicas=world_size, rank=rank)
+train_loader=DataLoader(train_dataset, batch_size=batch_size, shuffle=False,sampler=train_sampler,num_workers=workers)
+print('loading val data')
+val_sampler = DistributedSampler(val_dataset,num_replicas=world_size, rank=rank)
+val_loader=DataLoader(val_dataset, batch_size=batch_size, shuffle=False,sampler=val_sampler,num_workers=workers)
 print("All Datasets assigned")
 
-device = torch.device("cuda:0")
-torch.manual_seed(seed)
+device = torch.device(f"cuda:{local_rank}")
+torch.cuda.set_device(device)
+model=model.to(device)  
+
 with torch.cuda.amp.autocast():
-    summary(model,(4,192,192,144))
-model=torch.nn.DataParallel(model)
+    summary(model,(4,192,192,144))  
+torch.manual_seed(seed)    
+model=DistributedDataParallel(module=model, device_ids=[device],find_unused_parameters=False)
 optimiser = torch.optim.Adam(model.parameters(), lr, weight_decay=1e-5)
 scaler = torch.cuda.amp.GradScaler()
 print("Model defined and passed to GPU")
@@ -106,6 +146,9 @@ metric_values_wt = []
 metric_values_et = []
 
 total_start = time.time()
+
+
+
 
 print("starting epochs")
 for epoch in range(total_epochs):
@@ -183,7 +226,7 @@ for epoch in range(total_epochs):
                     if epoch>99:
                         torch.save(
                             model.state_dict(),
-                            os.path.join(root_dir, model_name+"CV"+str(fold_num)+"ms"+str(max_samples)+"rs"+str(seed)+method+'extra'))
+                            os.path.join(root_dir, model_name+str(total_start)+"CV"+str(fold_num)+"ms"+str(max_samples)+"rs"+str(seed)+method+'extra'))
                     else:
                     
                         torch.save(
@@ -193,7 +236,7 @@ for epoch in range(total_epochs):
                 else:
                     torch.save(
                         model.state_dict(),
-                        os.path.join(root_dir, date.today().isoformat()+'T'+str(datetime.today().hour)+ model_name),
+                        os.path.join(root_dir, date.today().isoformat()+'T'+str(total_start)+ model_name),
                     )
                 print("saved new best metric model")
                 
