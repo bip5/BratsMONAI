b04aa0f1f48a96bf47df692672828aed03a215df
bip5 <bip5@aber.ac.uk>
2023-09-15 21:42:55 +0100

    changed a few things, need to back up

--- scripts/Input/config.py
+++ scripts/Input/config.py
@@ -1,16 +1,32 @@
-root_dir="/scratch/a.bip5/BraTS 2021/"
-model_name='SegResNet' # pick from UNet SegResNet
-total_epochs=1200
+root_dir = "/scratch/a.bip5/BraTS 2021/BraTS21_data"
+model_name = 'SegResNet' # pick from UNet SegResNet
+total_epochs = 500
 val_interval = 1
 VAL_AMP = True
-load_save=0
-seed=0
-load_path='ENTER A PATH'
-fold_num=1
-max_samples=10
-method='A'#type of ensemble method
-upsample='DECONV' # upsample method in SegResNet
-batch_size=4
-T_max=4 #how often to reset cycling
-CV_flag=1
-lr=2e-4
+load_save = 1
+seed = 0
+load_path = '/scratch/a.bip5/BraTS 2021/BraTS21_data/SegResNetCV1ms1250rs0A'
+fold_num = 1
+max_samples = 1250
+method = 'A'#type of ensemble method
+upsample = 'DECONV' # upsample method in SegResNet
+batch_size = 4
+T_max = 100 #how often to reset cycling
+workers = 8
+lr = 0.0002
+fs_ensemble = 0 # fewshot ensemble
+exp_ensemble = 1 #'expert' ensemble
+CV_flag = 1 if fs_ensemble+exp_ensemble==0 else 0
+DDP = False
+train_partial = False
+unfreeze = 4 #set depending on how many items to unfreeze from layer list
+freeze_train=True # start freezing layers in training if freeze criteria met
+isolate_layer=True #whether to isolate one layer at a time while freeze training
+lr_cycling=False
+cluster_files ='/scratch/a.bip5/BraTS 2021/selected_files_seed1694621970MM.xlsx'
+################EVAL SPECIFIC VARIABLES#################
+eval_path = '/scratch/a.bip5/BraTS 2021/BraTS21_data/'+'2023-09-14T1694703670.5156782Cluster_0SegResNet' 
+
+eval_mode = 'cluster' #choose between ensemble, cluster and so on.
+
+# unused_collection=True if fs_ensemble==0 else False
--- scripts/Input/dataset.py
+++ scripts/Input/dataset.py
@@ -11,6 +11,7 @@ seed,
 )
 from torch.utils.data import Subset
 from monai.data import Dataset
+import pandas as pd
 
 
 IMG_EXTENSIONS = [
@@ -43,33 +44,150 @@ def make_dataset(data_dir):
                     if is_image_file(f):                  # check if expected extension
                         if re.search("seg",f):            # look for the mask files- have'seg' in the name 
                             masks.append(fpath)
+                            
                         else:
                             im_temp.append(fpath)         # all without seg are image files, store them in a list for each folder
-                images.append(im_temp)                    # add image files for each folder to a list
-                im_temp=[]
-    return images, masks
+                if im_temp:            
+                    images.append(im_temp)                    # add image files for each folder to a list
+                    im_temp=[]
     
+    
+    return images, masks
+
+
+def make_ens_dataset(path):
+    all_files = []
+    images=[]
+    masks=[]
+    im_temp=[]
+    folders=pd.read_csv(path)['mask_path']
+
+    for folder in folders:                    # for each folder
+         path=folder # combine root path with folder path
+         for root1, _, fnames in os.walk(path):       #list all file names in the folder         
+            for f in fnames:                          # go through each file name
+                fpath=os.path.join(root1,f)
+                if is_image_file(f):                  # check if expected extension
+                    if re.search("seg",f):            # look for the mask files- have'seg' in the name 
+                        masks.append(fpath)
+                    else:
+                        im_temp.append(fpath)         # all without seg are image files, store them in a list for each folder
+            if im_temp:            
+                    images.append(im_temp)                    # add image files for each folder to a list
+                    im_temp=[]
+    return images, masks
+
+def make_exp_dataset(path,sheet):
+    all_files = []
+    images=[]
+    masks=[]
+    im_temp=[]
+    folders=pd.read_excel(path,sheet)['Index']
+
+    for folder in folders:                    # for each folder
+         path=folder # combine root path with folder path
+         for root1, _, fnames in os.walk(path):       #list all file names in the folder         
+            for f in fnames:                          # go through each file name
+                fpath=os.path.join(root1,f)
+                if is_image_file(f):                  # check if expected extension
+                    if re.search("seg",f):            # look for the mask files- have'seg' in the name 
+                        masks.append(fpath)
+                    else:
+                        im_temp.append(fpath)         # all without seg are image files, store them in a list for each folder
+            if im_temp:            
+                    images.append(im_temp)                    # add image files for each folder to a list
+                    im_temp=[]
+    return images, masks
 
 indexes=np.random.choice(np.arange(max_samples),max_samples,replace=False)
 fold=int(max_samples/5)
 
 for i in range(1,6):
     if i==int(fold_num):
-        if i<5:
-            val_indices=indexes[(i-1)*fold:i*fold]
-            train_indices=np.delete(indexes,val_indices)#indexes[i*fold:(i+1)*fold]#
-        else:
-            val_indices=indexes[(i-1)*fold:i*fold]
-            train_indices=np.delete(indexes,val_indices)#indexes[(i-5)*fold:(i-4)*fold]
+        val_start=(i-1)*fold
+        val_end=val_start+100
+        test_start=val_end
+        test_end=i*fold       
+        
+        val_indices=indexes[val_start:val_end] 
+        test_indices=indexes[test_start:test_end]
+        # print(test_indices)
+        train_indices=np.delete(indexes,indexes[val_start:test_end])
             
            
            
 class BratsDataset(Dataset):
     def __init__(self,data_dir,transform=None):
         
-        self.image_list=make_dataset(data_dir)[0]
+        data=make_dataset(data_dir)
+        
+        self.image_list=data[0]
          
-        self.mask_list=make_dataset(data_dir)[1] 
+        self.mask_list=data[1]
+        self.transform=transform
+        
+    def __len__(self):
+#         return len(os.listdir(self.mask_dir))
+        return min(max_samples,len(self.mask_list))#
+    
+    def __getitem__(self,idx):
+        # print(idx)
+       
+        image=self.image_list[idx]
+       
+    
+        mask=self.mask_list[idx] 
+        
+
+            
+        item_dict={"image":image,"mask":mask}
+        # print(item_dict)
+        
+        if self.transform:
+            item_dict={"image":image,"mask": mask}
+            item_dict=self.transform(item_dict)
+            
+        
+        return item_dict
+
+
+class EnsembleDataset(Dataset):
+    def __init__(self,csv_path,transform=None):
+        
+        data=make_ens_dataset(csv_path)
+        self.image_list=data[0]
+         
+        self.mask_list=data[1] 
+        # print('files processed:' , self.mask_list)
+        self.transform=transform
+        
+    def __len__(self):
+#         return len(os.listdir(self.mask_dir))
+        return min(max_samples,len(self.mask_list))#
+    
+    def __getitem__(self,idx):
+        # print(idx)
+       
+        image=self.image_list[idx]
+    
+        mask=self.mask_list[idx] 
+
+            
+        item_dict={"image":image,"mask":mask}
+        
+        if self.transform:
+            item_dict={"image":image,"mask": mask}
+            item_dict=self.transform(item_dict)
+            
+        
+        return item_dict
+
+class ExpDataset(Dataset):
+    def __init__(self,path,sheet,transform=None):
+        
+        self.image_list,self.mask_list=make_exp_dataset(path,sheet)
+        
+        # print('files processed:' , self.mask_list)
         self.transform=transform
         
     def __len__(self):
@@ -94,6 +212,33 @@ class BratsDataset(Dataset):
         return item_dict
 
 
-
+class ExpDatasetEval(Dataset):
+    def __init__(self,path,sheet,transform=None):
+        
+        self.image_list,self.mask_list=make_exp_dataset(path,sheet)
+        
+        # print('files processed:' , self.mask_list)
+        self.transform=transform
+        
+    def __len__(self):
+#         return len(os.listdir(self.mask_dir))
+        return min(max_samples,len(self.mask_list))#
     
+    def __getitem__(self,idx):
+        # print(idx)
+       
+        image=self.image_list[idx]
+    
+        mask=self.mask_list[idx] 
+
+            
+        item_dict={"image":image,"label":mask}
+        # print(item_dict)
+        
+        if self.transform:
+            item_dict={"image":image,"label": mask}
+            item_dict=self.transform(item_dict)
+            
+        
+        return item_dict
     
