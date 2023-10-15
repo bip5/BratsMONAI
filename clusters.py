c7c50235037a6d8f2f6caf3ef60cd7c211bf453c
bip5 <bip5@aber.ac.uk>
2023-10-15 19:09:17 +0100

    bubble and heat plots

--- clusters.py
+++ clusters.py
@@ -9,6 +9,9 @@ import sys
 import matplotlib.pyplot as plt
 import time
 from scipy.spatial import distance
+import seaborn as sns
+from datetime import datetime
+import os
 
 
 parser=argparse.ArgumentParser(prog=sys.argv[0],description="Eval parser")
@@ -22,7 +25,9 @@ args=parser.parse_args()
 seed=args.seed
 np.random.seed(args.seed)
 # Assuming you have a DataFrame `df` with your data, and 'labels' column is not used in PCA
-df= pd.read_csv('Encoded_maxfeatures.csv')
+
+df= pd.read_csv('B23Encoded_maxfeatures.csv')
+# df= pd.read_csv('Dataset_features.csv')
 df.drop_duplicates(inplace=True)
 
 # df.drop(['a_centroid', 's_centroid','c_centroid'], axis=1, inplace=True)
@@ -31,6 +36,14 @@ df_features=df.drop(columns=['Unnamed: 0'])
 df_features.drop_duplicates(inplace=True)
 df_features.fillna(0,inplace=True)
 df_features.replace([np.inf, -np.inf], 0, inplace=True)
+
+    
+now = datetime.now()
+formatted_time =now.strftime('%Y-%m-%d_%H-%M-%S')
+plot_dir=os.path.join('./plots',formatted_time)
+
+os.makedirs(plot_dir,exist_ok=True)
+
 
 def normalise(df_features,normaliser=args.normaliser):
     if normaliser==0:
@@ -171,7 +184,7 @@ def sil_plots(df_features):
 def mahalanobis_distance(points, center):
     # Calculate the covariance matrix
     cov_matrix = np.cov(points, rowvar=False)
-    
+    corrcoef=np.corrcoef(points,rowvar=False)
     # Calculate the inverse of the covariance matrix
     inv_cov_matrix = np.linalg.inv(cov_matrix)
     
@@ -186,9 +199,58 @@ def mahalanobis_distance(points, center):
             dist=2
         distances.append(dist)
     
-    return distances
+    return distances,corrcoef
 
+def plot_correlations(cluster_number, corr_df):
+    # Assuming corr_df is the correlation matrix for the cluster
+    for col in corr_df.columns:
+        plt.figure(figsize=(12, 6))
+        sns.lineplot(x=corr_df.columns, y=corr_df[col])
+        plt.xticks(rotation=90)  # Rotate column labels for clarity
+        plt.title(f"Correlation of {col} with Other Features - Cluster {cluster_number}")
+        plt.ylabel(f"Correlation with {col}")
+        plt.tight_layout()  # Adjust layout for better visualization
+        plt.savefig(f"{plot_dir}/Cluster_{cluster_number}_Correlation_{col}.png")
+        plt.close()
+        
 
+def plot_heatmap(cluster_number, corr_df):
+    plt.figure(figsize=(12, 12))
+    sns.heatmap(corr_df, annot=False, cmap='coolwarm', vmin=-1, vmax=1)
+    plt.title(f"Correlation Heatmap - Cluster {cluster_number}")
+    plt.tight_layout()
+    plt.savefig(f"{plot_dir}/Cluster_{cluster_number}_Heatmap.png")
+    plt.close()
+
+def plot_pairgrid(cluster_number, subset_df):
+    g = sns.PairGrid(subset_df)
+    g.map_upper(plt.scatter, s=10)  # Scatter plot for upper grid
+    g.map_diag(sns.histplot)  # Histogram for diagonal
+    g.map_lower(sns.kdeplot)  # KDE for lower grid
+    g.savefig(f"{plot_dir}/Cluster_{cluster_number}_PairGrid.png")
+
+def plot_bubble_correlation(cluster_number, corr_df):
+    fig, ax = plt.subplots(figsize=(12, 12))
+    
+    # We'll use a mask to get the upper triangle (as correlation matrices are symmetric)
+    mask = np.triu(np.ones_like(corr_df, dtype=bool))
+    
+    for i, j in zip(*np.where(~mask)):
+        color = 'red' if corr_df.iloc[i, j] < 0 else 'blue'
+        size = abs(corr_df.iloc[i, j]) * 1000  # Scale size for better visualization
+        ax.scatter(i, j, s=size, c=color, marker='o', alpha=0.6)
+    
+    ax.set_xticks(np.arange(len(corr_df.columns)))
+    ax.set_yticks(np.arange(len(corr_df.columns)))
+    ax.set_xticklabels(corr_df.columns, rotation=90)
+    ax.set_yticklabels(corr_df.columns)
+    plt.title(f"Correlation Bubble Plot - Cluster {cluster_number}")
+    plt.tight_layout()
+    plt.savefig(f"{plot_dir}/Cluster_{cluster_number}_BubbleCorrelation.png")
+    plt.close()
+
+    
+        
 def save_cluster_files(df_features, n_clusters=4):
     print(df_features.duplicated().sum())
     features = normalise(df_features)
@@ -210,20 +272,12 @@ def save_cluster_files(df_features, n_clusters=4):
         cluster_points_indices = np.where(cluster_labels == i)[0]  # get global indices of all points in cluster i
         cluster_points = features[cluster_points_indices]  # get all points in cluster i
         print('cluster_points.shape',cluster_points.shape)
-        cluster_center = kmeans.cluster_centers_[i]  # get the center of cluster i
-        # cc=pd.Series(cluster_center,name='Cluster center')
-        # cc_dist = pd.Series([0],name='Cluster center')
-        # cc=cc_dist.append(cc,ignore_index=True)
-        # cc=pd.concat([cc_dist,cc]).reset_index(drop=True) #pd.concat appends row wise as axis=0 by default
-        # cc.append('Cluster center')
+        cluster_center = kmeans.cluster_centers_[i]  # get the center of cluster i   
         
-       
-        
-        distances = mahalanobis_distance(cluster_points, cluster_center)#[distance.euclidean(p, cluster_center) for p in cluster_points] 
+        distances,cov_matrix = mahalanobis_distance(cluster_points, cluster_center)#[distance.euclidean(p, cluster_center) for p in cluster_points] 
         # calculate distances
         
-        filepath = df['Unnamed: 0'].iloc[cluster_points_indices]#.str.slice(0, -16)
-        # filepath = df['mask_path'].iloc[cluster_points_indices].str.slice(0, -26)
+        filepath = df['Unnamed: 0'].iloc[cluster_points_indices]
         # Create a DataFrame
         df_sep = pd.DataFrame({
             'Distance': distances,
@@ -239,24 +293,27 @@ def save_cluster_files(df_features, n_clusters=4):
         features_df = pd.DataFrame(features, columns=df_features.columns[:])
         
         features_df['modified_mask_path'] = df['Unnamed: 0']#.str[:-26]
-        # features_df=pd.concat([cc,features_df]).reset_index(drop=True) # to add the cluster center
-        # print(features_df.columns)
-        # sys.exit()
-        # print(df_sep['Index'].iloc[0], features_df['modified_mask_path'].iloc[0])
-        # sys.exit()
+      
         # Merge the dataframes on the matching columns
         result_df = pd.merge(df_sep, features_df, left_on='Index', right_on='modified_mask_path', how='inner')
-       
+        
+        corr_df=pd.DataFrame(cov_matrix,index=df_features.columns[:], columns=df_features.columns[:])
+        plot_heatmap(i, corr_df)
+        # plot_bubble_correlation(i,corr_df)
+        # plot_pairgrid(i,corr_df)
         # Calculate mean and std dev for each feature in this cluster
         mean_series = result_df.mean(numeric_only=True)
         std_series = result_df.std(numeric_only=True)
         
+        
         # Add these as new columns to the mean and std dev dataframes
         mean_df[f'Cluster_{i}'] = mean_series
         std_df[f'Cluster_{i}'] = std_series
+       
         
         # Write each DataFrame to a specific sheet
         result_df.to_excel(writer, sheet_name=f'Cluster_{i}', index=False)
+        corr_df.to_excel(writer, sheet_name=f'ClusterCorr_{i}', index=True)
         
 
     # Write mean and std dev dataframes to separate sheets
