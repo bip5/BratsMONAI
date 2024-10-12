6303ecfb40827e7cd5c83e673b8147a8e06645c2
bip5 <bip5@aber.ac.uk>
2024-10-13 00:41:14 +0100

    Code snapshot for job 7952056

--- scripts/Training/training.py
+++ scripts/Training/training.py
@@ -500,7 +500,7 @@ def trainingfunc_simple(train_dataset, val_dataset,save_dir=save_dir,model=model
     
         if training_mode=='isles':
            train_dataset.transform = update_transforms_for_epoch(isles_list,epoch,total_epochs)
-            
+           train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,num_workers=workers ) 
         indices = list(range(1000))
         np.random.shuffle(indices)
         if use_sampler:
