2ee4ea5c8b902196bdc7efd67fb90764f1f7a2e4
bip5 <bip5@aber.ac.uk>
2024-10-13 13:56:36 +0100

    Code snapshot for job 7952118

--- scripts/Training/training.py
+++ scripts/Training/training.py
@@ -511,9 +511,9 @@ def trainingfunc_simple(train_dataset, val_dataset,save_dir=save_dir,model=model
            if epoch-best_metric_epoch>20:
                 if training_samples<230:
                     training_samples = len(train_indices)+10
-                    train_indices=indexes[:training_samples]
+                    new_indices=indexes[:training_samples]
                     full_train=IslesDataset("/scratch/a.bip5/BraTS/dataset-ISLES22^public^unzipped^version"  ,transform= train_transform_isles )
-                    train_dataset = Subset(full_train, train_indices)   
+                    train_dataset = Subset(full_train, new_indices)   
                     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=workers ) 
                     
                 
