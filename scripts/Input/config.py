0509ba31c803c58a6ade48322b9f18345d9f1cf4
bip5 <bip5@aber.ac.uk>
2024-10-13 01:29:28 +0100

    Code snapshot for job 7952065

--- scripts/Input/config.py
+++ scripts/Input/config.py
@@ -156,7 +156,7 @@ elif mode_index==19:
     batch_size=2
     in_channels = 2
     out_channels = 1
-    training_samples=230
+    training_samples=50
     lr = 0.0002
     activation = 'RELU'
     loss_type = 'DiceFocal'
