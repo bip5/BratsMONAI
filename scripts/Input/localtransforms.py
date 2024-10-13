7a50272a29b2eb556942e269f5a66578b889a108
bip5 <bip5@aber.ac.uk>
2024-10-13 01:41:02 +0100

    Code snapshot for job 7952068

--- scripts/Input/localtransforms.py
+++ scripts/Input/localtransforms.py
@@ -688,8 +688,10 @@ class DynamicProbabilityTransform:
         # Example linear scaling of probability
         self.current_prob = self.base_prob + ((1.0 - self.base_prob) )#* (epoch / max_epochs))
 
-    def __call__(self, x):   
-        if random.random() < self.current_prob:
+    def __call__(self, x):  
+        rn= random.random()
+        print(rn)
+        if rn < self.current_prob:
             return self.transform(x)
         return x
 
@@ -707,5 +709,5 @@ def update_transforms_for_epoch(x_transform, epoch, max_epochs):
     
     # Rebuild the compose transform pipeline with updated probabilities
     x_transform = Compose(transform_list)
-    print('new transform')
+
     return x_transform
