76bfd02e02c9dbbc5b68a7392fd6ea19fc868a4a
bip5 <bip5@aber.ac.uk>
2024-10-19 22:23:39 +0100

    Code snapshot for job 7953653

--- scripts/Input/localtransforms.py
+++ scripts/Input/localtransforms.py
@@ -369,7 +369,7 @@ isles_list = [
         mode=("bilinear", "nearest"),
         padding_mode="border",
     ),
-    RandFlipd(keys=["image", "mask"], spatial_axis=[0, 1, 2], prob=0.2),
+    # RandFlipd(keys=["image", "mask"], spatial_axis=[0, 1, 2], prob=0.2),
     # RandGaussianNoised(keys="image", prob=0.3, mean=0.0, std=0.1),
     # RandGaussianSmoothd(
         # keys="image",
