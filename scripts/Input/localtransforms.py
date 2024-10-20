74b9c0cb0bed32f800991838faa28e258392fec4
bip5 <bip5@aber.ac.uk>
2024-10-20 12:44:09 +0100

    Code snapshot for job 7953775

--- scripts/Input/localtransforms.py
+++ scripts/Input/localtransforms.py
@@ -355,8 +355,8 @@ isles_list = [
     EnsureTyped(keys=["image", "mask"]),
     RandAffined(
         keys=["image", "mask"],
-        prob=0.5,
-        rotate_range=(np.pi/12, np.pi/12, np.pi/12),
+        prob=0.3,
+        rotate_range=(np.pi/6, np.pi/6, np.pi/6),
         scale_range=(0.1, 0.1, 0.1),
         mode=("bilinear", "nearest"),
         padding_mode="border",
