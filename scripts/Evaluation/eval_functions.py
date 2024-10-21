0fa1e6eb90190bd2e2f623183399929e8f4c2b1b
bip5 <bip5@aber.ac.uk>
2024-10-21 20:43:10 +0100

    Code snapshot for job 7954113

--- scripts/Evaluation/eval_functions.py
+++ scripts/Evaluation/eval_functions.py
@@ -75,9 +75,8 @@ def model_loader(modelweight_path,scaler,model_id=model_name,train=False,lr_sche
     model = create_model(model_id)
     try:
         # Load the state dict from the file
-        checkpoint = torch.load(modelweight_path)
-    except:
         checkpoint = torch.jit.load(modelweight_path)
+    
         
     def wrap_model(state_dict,model):
         # Check if the state dict contains keys prefixed with 'module.'
