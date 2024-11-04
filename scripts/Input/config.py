0e854a8df5b535406d3e023d9c276d7491cd4a34
bip5 <bip5@aber.ac.uk>
2024-11-04 01:07:16 +0000

    Code snapshot for job 7957283

--- scripts/Input/config.py
+++ scripts/Input/config.py
@@ -149,21 +149,21 @@ elif mode_index==18:
     model_name='PixelLayer'
     activation= 'hardswish' #'RELU'# 
 elif mode_index==19:
-    load_save = 0
-    jit_model=False
-    load_path = '/scratch/a.bip5/BraTS/weights/job_7956992/2024-11-02SegResNetDS_j7956992_ts0' #'/scratch/a.bip5/BraTS/weights/job_7953765/2024-10-20SegResNetDS_j7953765_ts0_LL' #'/scratch/a.bip5/BraTS/weights/0_NVAUTO_models/model14.ts'  # 
+    load_save = 1
+    jit_model=True
+    load_path = '/scratch/a.bip5/BraTS/weights/0_NVAUTO_models/model14.ts'  # '/scratch/a.bip5/BraTS/weights/job_7956992/2024-11-02SegResNetDS_j7956992_ts0' #'/scratch/a.bip5/BraTS/weights/job_7953765/2024-10-20SegResNetDS_j7953765_ts0_LL' #
     root_dir = '/scratch/a.bip5/BraTS/dataset-ISLES22^public^unzipped^version' 
     model_name = 'SegResNetDS'
     training_mode = 'isles'
     max_samples = 250
     roi = (192,192,128)
-    total_epochs = 125
+    total_epochs = 1000
     init_filter_number= 32
     batch_size=1
     in_channels = 2
     out_channels = 2    
     inf_overlap = 0.7
-    lr = 0.0002
+    lr = 0.000025
     activation = 'RELU'
     loss_type = 'DiceFocal' #'dice'#
     seed = 1111
