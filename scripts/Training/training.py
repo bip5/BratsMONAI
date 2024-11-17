97f2e94afb79d1d7c4b2ce7d40f9131927da9c49
bip5 <bip5@aber.ac.uk>
2024-11-17 00:25:46 +0000

    Code snapshot for job 7959377

--- scripts/Training/training.py
+++ scripts/Training/training.py
@@ -549,7 +549,7 @@ def trainingfunc_simple(train_dataset, val_dataset,save_dir=save_dir,model=model
                 if epoch==0:
                     transform_list= isles_list[:-exclusion_counter]
                     # updated_transform_isles = update_transforms_for_epoch(transform_list,init_loss=1,best_loss=1,patience=1)
-                    full_train=IslesDataset("/scratch/a.bip5/BraTS/dataset-ISLES22^public^unzipped^version"  ,transform= train_transform )
+                    full_train=IslesDataset("/scratch/a.bip5/BraTS/dataset-ISLES22^public^unzipped^version"  ,transform= train_transform_isles )
                     train_dataset = Subset(full_train, train_indices)   # okay since train indices=230 on load_save
                     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,num_workers=workers ) 
                     
