af5835cdcc794626845cb3474b2aa4826afdf99c
bip5 <bip5@aber.ac.uk>
2024-11-21 22:51:54 +0000

    Code snapshot for job 7960413

--- scripts/Input/config.py
+++ scripts/Input/config.py
@@ -151,20 +151,20 @@ elif mode_index==18:
 elif mode_index==19:
     load_save = 1    
     
-    load_path = '/scratch/a.bip5/BraTS/weights/job_7960401/2024-11-21SegResNetDS_j7960401_ts0' #'/scratch/a.bip5/BraTS/dataset-ISLES22^public^unzipped^version/auto3d_scripts/full_set_of_files/segresnet_0/model/model_final.pt'#'/scratch/a.bip5/BraTS/weights/job_7957288/2024-11-04SegResNetDS_j7957288_ts0'#'/scratch/a.bip5/BraTS/weights/0_NVAUTO_models/model14.ts'  # '/scratch/a.bip5/BraTS/weights/job_7953765/2024-10-20SegResNetDS_j7953765_ts0' #
+    load_path = '/scratch/a.bip5/BraTS/dataset-ISLES22^public^unzipped^version/auto3d_scripts/full_set_of_files/segresnet_0/model/model_final.pt'#'/scratch/a.bip5/BraTS/weights/job_7957288/2024-11-04SegResNetDS_j7957288_ts0'#'/scratch/a.bip5/BraTS/weights/0_NVAUTO_models/model14.ts'  # '/scratch/a.bip5/BraTS/weights/job_7960401/2024-11-21SegResNetDS_j7960401_ts0' #'/scratch/a.bip5/BraTS/weights/job_7953765/2024-10-20SegResNetDS_j7953765_ts0' #
     jit_model= False
     root_dir = '/scratch/a.bip5/BraTS/dataset-ISLES22^public^unzipped^version' 
     model_name = 'SegResNetDS'
     training_mode = 'isles'
     max_samples = 250
     roi = (192,192,128)
-    total_epochs = 40
+    total_epochs = 100
     init_filter_number= 32
     batch_size=1
     in_channels = 2
     out_channels = 2    
     inf_overlap = 0.625
-    lr = 0.0001
+    lr = 0.00005
     activation = 'RELU'
     loss_type = 'DiceFocal' #'dice'#
     seed = 1
